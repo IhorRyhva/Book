723bf95db9727aac906d8943c1f3e9525ced90f7
Admin <rykhvaihor@gmail.com>
2024-12-14 23:23:17 +0200

    UPDATE

---
+++ services/Booking/src/main/java/com/mtFirstProject/Booking/Email.java
@@ -0,0 +1,21 @@
+package com.mtFirstProject.Booking;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Email{
+    private final JavaMailSender mailSender;
+    public void sendEmail(String to, String subject, String message) {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("rykhvaihor@gmail.com");
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+
+        this.mailSender.send(simpleMailMessage);
+    }
+}
