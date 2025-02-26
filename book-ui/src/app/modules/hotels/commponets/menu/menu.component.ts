f19a7e74f8b0ab61bce3eed018c7878039fe89b1
Admin <rykhvaihor@gmail.com>
2025-02-26 15:53:04 +0200

    UPDATE 8

---
+++ book-ui/src/app/modules/hotels/commponets/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-menu',
+  imports: [FormsModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
